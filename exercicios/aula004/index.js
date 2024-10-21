ea01bb89a89d12ef8e80f344e264bb07bcd91dc3
Andrei Gravonski <gravonskia@gmail.com>
2024-10-20 23:51:43 -0300

    criação de documento HTML com JS

---
+++ exercicios/aula004/index.js
@@ -0,0 +1,10 @@
+//linha de comentário
+console.log("Olá, Mundo!");
+console.log("Linha 1");
+console.log("Linha 2");
+console.log("Linha 3");
+console.log("Linha 4");
+
+/*bloco
+de
+comentário*/
---
+++ exercicios/aula005/js/index.js
@@ -0,0 +1,2 @@
+console.log("Olá, Mundo!");
+console.log("Este trecho de texto será exibido no console do navegador");
