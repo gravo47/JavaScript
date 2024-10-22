d0713e64ffd6a1e65ffeb6abf509a612dbdc9878
Andrei Gravonski <gravonskia@gmail.com>
2024-10-22 01:37:40 -0300

    Variáveis e Constantes em JS

---
+++ exercicios/aula007/index.js
@@ -0,0 +1,7 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+const resultadoTriplicado = resultado * 3;
+
+console.log(resultadoTriplicado);
