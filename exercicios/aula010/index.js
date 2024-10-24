33df2676bc0b8e15c6ee6c9d4ecea1f1b2c7fddf
Andrei Gravonski <gravonskia@gmail.com>
2024-10-24 01:49:08 -0300

    exercícios com operadores aritméticos

---
+++ exercicios/aula010/index.js
@@ -0,0 +1,23 @@
+/*
+let contador = 1;
+contador++;
+contador++;
+contador++;
+contador++;
+contador++;
+console.log(contador);
+contador--
+console.log(contador);
+*/
+const step = 5;
+let contador = 0;
+contador = contador + step;
+console.log(contador);
+contador = contador + step;
+console.log(contador);
+contador = contador + step;
+console.log(contador);
+contador = contador + step;
+console.log(contador);
+contador = contador + step;
+console.log(contador);
