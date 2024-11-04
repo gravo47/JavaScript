20c3f93c1a5d1853acc6a6598ba93bfe7cb6234e
Andrei Gravonski <gravonskia@gmail.com>
2024-11-03 22:46:46 -0300

    Muitos exercicios

---
+++ exercicios/aula014/index.js
@@ -0,0 +1,11 @@
+let num1 = 0.5;
+let num2 = 0.4;
+
+//console.log(num1.toString() + num2);
+//console.log(num1.toString(2));
+//console.log(num2.toFixed(2));
+//console.log(Number.isInteger(num2));
+/*const res = num1 + num2;
+console.log(res);
+console.log(Number.isInteger(res));*/
+console.log(num1 + num2);
