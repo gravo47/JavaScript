cd0fd22db2d354ca261b990c66aba7b59803cd00
Andrei Gravonski <gravonskia@gmail.com>
2024-11-05 13:08:52 -0300

    exercícios com arrays

---
+++ exercicios/aula016/index.js
@@ -0,0 +1,9 @@
+const alunos = ['Andrei', 'Anthony', 'Maria'];
+
+console.log(alunos);
+
+//console.log(alunos.slice(1, 3));
+//alunos[2] = 'Julia';
+//alunos[3] = 'Lucas';
+//alunos.unshift('Luiza');
+//alunos.push('Luiza');
