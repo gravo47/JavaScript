67ed58373d685a64590db42bf19bd1af1c2e8186
Andrei Gravonski <gravonskia@gmail.com>
2024-11-20 22:43:52 -0300

    Criação de formulário com arrays, objetos e mais

--- exercicios/aula20/js/script.js
+++ exercicios/aula20/js/script.js
@@ -1,10 +1,32 @@
 function meuEscopo () {
     const form = document.querySelector('.form');
+    const resultados = document.querySelector('.resultados');
 
+    const pessoas = [];
+    
     function recebaEventoForm (evento) {
-        
-    }
+        evento.preventDefault();
 
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+
+        console.log(pessoas);
+
+        resultados.innerHTML += `<p>${nome.value} ${sobrenome.value}` + `${peso.value} ${altura.value}</p>`;
+
+        console.log(nome.value, sobrenome.value, peso.value, altura.value);
+
+    }
+    
     form.addEventListener('submit', recebaEventoForm);
 }
 meuEscopo();
