e7a833fa2710571ee3a932c9016b1e8a91a7b67b
Andrei Gravonski <gravonskia@gmail.com>
2024-11-24 21:09:34 -0300

    criação de exercícios com avaliações de curto-circuito

---
+++ exercicios/aula022/index.js
@@ -0,0 +1,10 @@
+/*const expr = true && true && false;
+console.log(expr);*/
+
+/*const usuario = 'Luiz';
+const senha = '123456';
+//                     true                 false
+const vaiLogar = usuario === 'Luiz' && senha === '12345';
+console.log(vaiLogar);*/
+
+console.log(!!!!true);
---
+++ exercicios/aula023/index.js
@@ -0,0 +1,23 @@
+/*function falaOi () {
+    return 'Oi';
+}
+
+const vaiExe =  false;
+
+console.log(falaOi() && vaiExe);*/
+
+/*console.log(0 || false || '' || '' || undefined || 98);
+console.log(0 || false || '' || 'Luiz' || undefined || 98);*/
+
+/*const corUsu = 'preto';
+const corPad = corUsu || 'azul';
+
+console.log(corPad);*/
+
+const a = 0;
+const b = false;
+const c = NaN;
+const d = 'false'; //valor que foi exebido no console
+const e = undefined;
+
+console.log(a || b || c || d || e);
