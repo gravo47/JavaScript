1e6e4fc3c5cac30d3ba6da48696283bcb3f7ca46
Andrei Gravonski <gravonskia@gmail.com>
2024-12-04 23:08:56 -0300

    Atualização function IMC com JS

--- exercicios/aula026/assets/js/main.js
+++ exercicios/aula026/assets/js/main.js
@@ -2,10 +2,40 @@ const form = document.querySelector('#form');
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
-    console.log('Evento Previnido!')
+    const inputPeso = e.target.querySelector('#pesoImc');
+    const inputAltura = e.target.querySelector('#alturaImc');
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso) {
+        setResultado('Peso inválido!', false);
+        return;
+    }
+
+    if (!altura) {
+        setResultado('Altura inválida!', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+
+    console.log('Cheguei aqui!');
 });
 
-function setResultado(msg) {
-    const resultado = document.querySelector('#resultado')
-    resultado.innerHTML = msg
+function getImc (peso, altura){
+    const imc = peso / altura **2;
+    return imc.toFixed(2);
+}
+
+function criaP () {
+    const p = document.createElement('p');
+    return p;
+}
+
+function setResultado(msg, isValid) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+    const p = criaP();
+    p.innerHTML = msg;
+    resultado.appendChild(p);
 }
