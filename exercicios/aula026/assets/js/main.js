bf28470b65d206b27a7bad041d9ec47423f26ebe
Andrei Gravonski <gravonskia@gmail.com>
2024-12-05 22:23:32 -0300

    finalização de formulário HTML, CSS e JavaScript

--- exercicios/aula026/assets/js/main.js
+++ exercicios/aula026/assets/js/main.js
@@ -18,9 +18,23 @@ form.addEventListener('submit', function(e){
     }
 
     const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
 
-    console.log('Cheguei aqui!');
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+
+    setResultado(msg, true);
 });
+
+function getNivelImc (imc) {
+    const nivel = ['Abaixo do peso', 'Peso Normal', 'Sobrepeso', 'Obesidade Grau 1', 'Obesidade Grau 2', 'Obesidade Grau 3']
+
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
+}
 
 function getImc (peso, altura){
     const imc = peso / altura **2;
@@ -35,7 +49,14 @@ function criaP () {
 function setResultado(msg, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
+
     const p = criaP();
+    if (isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
+
     p.innerHTML = msg;
     resultado.appendChild(p);
 }
