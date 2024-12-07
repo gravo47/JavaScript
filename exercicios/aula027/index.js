762ca9cc1f7bf9a55b95c8fdf94ac6314dbe0241
Andrei Gravonski <gravonskia@gmail.com>
2024-12-06 23:01:53 -0300

    exercícios com operações ternárias

---
+++ exercicios/aula027/index.js
@@ -0,0 +1,10 @@
+const pontUsuario = 9958;
+const nivelUsuario = pontUsuario >= 1000 ? 'Usuário VIP' : 'Usuário Comum';
+console.log(nivelUsuario);
+
+/*
+if (pontUsuario >= 1000) {
+    console.log('Usuário VIP');
+} else {
+    console.log('Usuário Comum');
+} */
---
+++ exercicios/aula028/index.js
@@ -0,0 +1,11 @@
+const data = new Date('2024-11-01 14:00:05');
+console.log('Dia', data.getDate());
+console.log('Ano', data.getFullYear());
+console.log('Mês', data.getMonth() + 1);
+console.log('Hora', data.getHours());
+console.log('Minuto', data.getMinutes());
+console.log('Segundo', data.getSeconds());
+console.log('Milisegundo', data.getMilliseconds());
+console.log('Dia da semana', data.getDay());
+console.log(data.toString());
+
