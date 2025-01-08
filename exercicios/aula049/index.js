2b757720d2e7453b9247c34acaa8807de079fab3
Andrei Gravonski <gravonskia@gmail.com>
2025-01-07 22:55:26 -0300

    Funções em JS

---
+++ exercicios/aula049/index.js
@@ -0,0 +1,17 @@
+// function falaOi() {
+//     console.log('Oi')
+// };
+
+// falaOi();
+
+const dados = function() {
+    console.log('test')
+};
+
+dados();
+
+const arrowFunc = () => {
+    console.log('test 2')
+};
+
+arrowFunc();
