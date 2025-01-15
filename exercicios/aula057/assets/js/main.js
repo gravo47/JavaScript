ed0619a91ac49e256f3b10839cd2685c14b9c11e
Andrei Gravonski <gravonskia@gmail.com>
2025-01-14 21:24:14 -0300

    finalização do projeto calculadora

--- exercicios/aula057/assets/js/main.js
+++ exercicios/aula057/assets/js/main.js
@@ -1,24 +1,69 @@
 function criaCalculadora() {
     return {
         display: document.querySelector('.display'),
-
-
-
-        inicia: function(){
+        btnClear: document.querySelector('.btn-clear'),
+        
+        inicia(){
             this.cliqueBotoes();
+            this.enterPress();
+        },
+
+        enterPress(){
+            this.display.addEventListener('keyup', e => {
+                if (e.keyCode === 13) {
+                    this.realizaConta();
+                }
+            })
+        },
+        
+        realizaConta(){
+            let conta = this.display.value;
+
+            try {
+                conta = eval(conta);
+                if (!conta) {
+                    alert('Conta Inválida!')
+                    return;
+                }
+
+                this.display.value = String(conta);
+            } catch(e) {
+                alert('Conta Inválida!')
+                return;
+            }
+        },
+
+        clearDisplay(){
+            this.display.value = ''
+        },
+
+        deleteOne(){
+            this.display.value = this.display.value.slice(0, -1);
         },
 
         cliqueBotoes() {
             document.addEventListener('click', function(e){
                 const el = e.target;
-                if (el.classList.contains('btn-num')) {
-                    this.btnParaDisplay()
+                if (el.classList.contains('btn-num')){
+                    this.btnParaDisplay(el.innerText);
+                }
+
+                if (el.classList.contains('btn-clear')){
+                    this.clearDisplay();
+                }
+
+                if (el.classList.contains('btn-del')){
+                    this.deleteOne();
+                }
+
+                if (el.classList.contains('btn-eq')){
+                    this.realizaConta();
                 }
             }.bind(this));
         },
 
         btnParaDisplay(valor) {
-
+            this.display.value += valor;
         }
     };
 };
