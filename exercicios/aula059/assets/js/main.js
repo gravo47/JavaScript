f8c1c38f41bc9f5c803479bf57737e699e817f6a
Andrei Gravonski <gravonskia@gmail.com>
2025-01-15 21:59:42 -0300

    Calculadora com arrows functions

--- exercicios/aula059/assets/js/main.js
+++ exercicios/aula059/assets/js/main.js
@@ -2,20 +2,20 @@ function Calculadora() {
         this.display = document.querySelector('.display');
         this.btnClear = document.querySelector('.btn-clear');
         
-        this.inicia = function() {
+        this.inicia = () => {
             this.cliqueBotoes();
             this.enterPress();
         };
 
-        this.enterDel = function() {
-            this.display.addEventListener('keyup', e => {
+        this.enterDel = () => {
+            this.document.addEventListener('keyup', e => {
                 if (e.keyCode === 8) {
                     this.clearDisplay();
                 }
             });
         };
 
-        this.enterPress = function() {
+        this.enterPress = () => {
             this.display.addEventListener('keyup', e => {
                 if (e.keyCode === 13) {
                     this.realizaConta();
@@ -23,7 +23,7 @@ function Calculadora() {
             });
         };
         
-        this.realizaConta = function() {
+        this.realizaConta = () => {
             let conta = this.display.value;
 
             try {
@@ -40,34 +40,32 @@ function Calculadora() {
             }
         };
 
-        this.clearDisplay = function() {
-            this.display.value = ''
-        };
+        this.clearDisplay = () => this.display.value = '';
 
-        this.deleteOne = function() {
-            this.display.value = this.display.value.slice(0, -1);
-        };
+        this.deleteOne = () => this.display.value = this.display.value.slice(0, -1);
 
-        this.cliqueBotoes = function() {
-            document.addEventListener('click', function(e){
+        this.cliqueBotoes = () => {
+            document.addEventListener('click', (e) => {
                 const el = e.target;
-                if (el.classList.contains('btn-num')){
+                if (el.classList.contains('btn-num')) {
                     this.btnParaDisplay(el.innerText);
+                    this.display.focus();
                 }
-
-                if (el.classList.contains('btn-clear')){
+        
+                if (el.classList.contains('btn-clear')) {
                     this.clearDisplay();
                 }
-
-                if (el.classList.contains('btn-del')){
+        
+                if (el.classList.contains('btn-del')) {
                     this.deleteOne();
                 }
-
-                if (el.classList.contains('btn-eq')){
+        
+                if (el.classList.contains('btn-eq')) {
                     this.realizaConta();
                 }
-            }.bind(this));
+            });
         };
+        
 
         this.btnParaDisplay= function(valor) {
             this.display.value += valor;
